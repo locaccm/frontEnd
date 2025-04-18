ba808e76c4b5baac1c7765535398bf8d59cf2513
luckytzu <tatindylan@gmail.com>
2025-04-18 21:06:39 +0200

    add: Components chat bubble

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import ChatBubble from '../components/ChatBubble.js'
+import '../styles/ChatBubble.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <ChatBubble />
+      </div>
+    </>
+  )
+}
+
+export default App
