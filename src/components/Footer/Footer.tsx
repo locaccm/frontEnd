005b9ed26c9bc8d2b1a7bcb61bbc04d751a62248
moslehSN <mosleh.snoussi49@gmail.com>
2025-04-18 23:08:40 +0200

    feat: [AIC-51] - Landing page and authentication frontend

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => (
+  <footer className="footer">
+    © 2025 LocaCCM. Tous droits réservés.
+  </footer>
+);
+
+export default Footer;
