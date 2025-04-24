0eea1393e19f751cb958540c3ef950aea68eecb2
moslehSN <mosleh.snoussi49@gmail.com>
2025-04-24 15:39:09 +0200

    fix: [AIC-51] - fix tests2

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom", 
   transform: {
     "^.+\\.tsx?$": ["ts-jest", {}],
+  },
+  moduleNameMapper: {
+    // Ignore les fichiers CSS
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    
+    // Ignore les images 
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
   },
 };
