f8eb8e9563e758c4d019476e0ca85aa414d354d5
moslehSN <mosleh.snoussi49@gmail.com>
2025-04-24 10:51:29 +0200

    fix: [AIC-51] - add test

---
+++ src/testes/a.test.ts
@@ -0,0 +1,8 @@
+// if i don't create this file i can't push the repository 
+
+//Small test to complete the CI with jtest
+//Can't pass the CI without a test
+test("sum of 1 + 2", () => {
+    expect(1 + 2).toBe(3);
+  });
+  
