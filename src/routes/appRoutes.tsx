06705b5dd676e877a747e08185a5744512cd3068
leojet02 <leojet02100@live.fr>
2025-06-11 19:53:20 +0200

    feature: [AIC-50] chat UI with user fetch, contact list, and message flow

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../pages/authentication/Signup/Signup.js";
 import Signin from "../pages/authentication/Signin/Signin.js";
+import ChatBubble from "../pages/chatManagement/ChatManagement.js";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/chat" element={<ChatBubble />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
