0d3018c1a069fb1fe2649337cd424165364367cc
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-06-15 14:41:14 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -102,6 +102,4 @@ app.get(/^\/(?!api|files|upload).*/, (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT}`);
-});
+app.listen(PORT, () => {});
