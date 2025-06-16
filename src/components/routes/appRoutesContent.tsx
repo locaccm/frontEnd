f32277c37532c69a62756ba0da77e51dd76d5ed4
TOLOES <tomdehame@orange.fr>
2025-06-16 11:56:03 +0200

    fix: [AIC-???] - fix tab display

--- src/components/routes/appRoutesContent.tsx
+++ src/components/routes/appRoutesContent.tsx
@@ -10,26 +10,33 @@ import DocumentWrapper from "../../routes/DocumentWrapper.js";
 import ChatBubble from "../../pages/chatManagement/chatManagement.js";
 import MainLayout from "../../layout/MainLayout.js";
 import Home from "../../pages/home.js";
+import {usePageTitle} from "../../hooks/manageNamePage.js";
 
-const AppRoutesContent = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/signin" element={<Signin />} />
-    <Route path="/signup" element={<Signup />} />
-    <Route element={
-      <ProtectedRoute>
-        <MainLayout />
-      </ProtectedRoute>
-    }>
-      <Route path="/profile" element={<ProfileManagement />} />
-      <Route path="/leases" element={<HousingManagement />} />
-      <Route path="/wealth-management" element={<WealthManagement />} />
-      <Route path="/document-management/:leaseId" element={<DocumentWrapper />} />
-      <Route path="/contacts" element={<ChatBubble />} />
-      <Route path="/calendar" element={<div>Calendar Page</div>} />
-      <Route path="/logout" element={<Logout />} />
-    </Route>
-  </Routes>
-);
+const AppRoutesContent = () => {
+    usePageTitle();
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+                element={
+                    <ProtectedRoute>
+                        <MainLayout />
+                    </ProtectedRoute>
+                }
+            >
+                <Route path="/profile" element={<ProfileManagement />} />
+                <Route path="/leases" element={<HousingManagement />} />
+                <Route path="/wealth-management" element={<WealthManagement />} />
+                <Route path="/document-management/:leaseId" element={<DocumentWrapper />} />
+                <Route path="/contacts" element={<ChatBubble />} />
+                <Route path="/calendar" element={<div>Calendar Page</div>} />
+                <Route path="/logout" element={<Logout />} />
+            </Route>
+        </Routes>
+    );
+};
 
 export default AppRoutesContent;
